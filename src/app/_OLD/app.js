4e95ae9a04be429bcfa33f50251e142729faa3f1
Wesley Lima <wesley@thwess.com>
2014-03-04 18:18:50 -0300

    Insert angular and grunt

--- src/app/_OLD/app.js
+++ src/app/_OLD/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('youuuApp', [
+angular.module('yoAngularIonicApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
--- src/app/_OLD/controllers/main.js
+++ src/app/_OLD/controllers/main.js
@@ -1,10 +1,4 @@
 'use strict';
 
-angular.module('youuuApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('yoAngularIonicApp')
+  .controller('MainCtrl', function () {});
